7526aa3a3300455302d85db1c937c34d1a7947a6
EdiMarian <ichimedimarian@gmail.com>
2023-03-20 16:26:26 +0200

    feature: set-sfts-reward add

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           16
+// Endpoints:                           17
 // Async Callback (empty):               1
-// Total number of exported functions:  18
+// Total number of exported functions:  19
 
 #![no_std]
 #![feature(alloc_error_handler, lang_items)]
@@ -19,6 +19,7 @@ multiversx_sc_wasm_adapter::endpoints! {
     stake
     (
         togglePause
+        setSftsReward
         fundSystem
         withdrawFunds
         stake
